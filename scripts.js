109dfe6d0c376b393ec568400f96ae74b727e06f
rajeshdn <codingmarket@gmail.com>
2022-03-07 14:14:48 +0530

    files added

---
+++ scripts.js
@@ -0,0 +1,13 @@
+var search_btn = document.querySelector(".search_btn");
+var close_btn = document.querySelector(".close_btn");
+var search_box_wrap = document.querySelector(".search_box_wrap");
+var input_search = document.querySelector(".input_search");
+
+search_btn.addEventListener("click", function(){
+	search_box_wrap.classList.add("active");
+	input_search.focus();
+});
+
+close_btn.addEventListener("click", function(){
+	search_box_wrap.classList.remove("active");
+});
